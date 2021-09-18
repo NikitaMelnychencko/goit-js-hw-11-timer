86e47a6f3718a01dd95e684d547bbd291eef43e7
NikitaMelnychencko <hunter6059@gmail.com>
2021-09-18 20:07:48 +0300

    fix

--- src/js/modual-1.js
+++ src/js/modual-1.js
@@ -1,10 +1,10 @@
 
 class CountdownTimer{
-  constructor({ selector,classUpdateValue, targetDate, onTick }) {
+  constructor({ selector,classUpdateValue, targetDate }) {
     this.selector = selector;
     this.targetDate = targetDate;
     this._refs = this._getRefs(selector,classUpdateValue);
-    this.onTick = onTick;
+    
 
     this.start();
   }
@@ -20,13 +20,46 @@ class CountdownTimer{
     let deltaTime = currentTime - startTime;
     setInterval(() => {
       deltaTime = deltaTime - 1000;
-      const { days, hours, mins, secs } = getTimeComponents(deltaTime);
+      const { days, hours, mins, secs } = this.getTimeComponents(deltaTime);
       const arr=[days,hours,mins,secs]
       
 
-      this.onTick(arr,this._refs.controls);
+      this.updateClockFace(arr,this._refs.controls);
     }
     ,1000)
+  }
+
+  getTimeComponents(time) {
+  /*
+  * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
+  * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
+  */
+  const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+  /*
+  * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
+  * остатка % и делим его на количество миллисекунд в одном часе
+  * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
+  */
+  const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+  /*
+  * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
+  * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
+  */
+  const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+  /*
+  * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
+  * миллисекунд в одной секунде (1000)
+  */
+  const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+  return {days,hours,mins,secs}
+  }
+   pad(value) {
+  return String(value).padStart(2, '0');
+  }
+  updateClockFace(arr,bloks) {
+  bloks.forEach((elem,index) => {
+    elem.textContent = arr[index];
+  });
   }
 }
 
@@ -34,38 +67,5 @@ const countdownTimer = new CountdownTimer({
   selector: '#timer-1',
   classUpdateValue: 'value',
   targetDate: new Date('Jul 17, 2019'),
-  onTick: updateClockFace,
 });
 
-function getTimeComponents(time) {
-  /*
-  * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
-  * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
-  */
-  const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-  /*
-  * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
-  * остатка % и делим его на количество миллисекунд в одном часе
-  * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
-  */
-  const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-  /*
-  * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
-  * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
-  */
-  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-  /*
-  * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
-  * миллисекунд в одной секунде (1000)
-  */
-  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
-  return {days,hours,mins,secs}
-}
-function pad(value) {
-  return String(value).padStart(2, '0');
-}
-function updateClockFace(arr,bloks) {
-  bloks.forEach((elem,index) => {
-    elem.textContent = arr[index];
-  });
-}
